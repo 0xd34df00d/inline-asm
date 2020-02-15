67dc7febe599f778b469d5489f48e2f598067330
0xd34df00d <0xd34df00d@gmail.com>
2020-02-15 15:42:58 -0500

    Yay it even works!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MagicHash, GHCForeignImportPrim, UnliftedFFITypes #-}
+
 module Main where
+
+import GHC.Prim
+import GHC.Word
 
 import Language.Asm.Inline
 
+inlineAsm ".global foobar\nfoobar:\tadd %rbx, %rbx"
+
+foreign import prim "foobar" foobar :: Word# -> Word#
+
+foobar' :: Word -> Word
+foobar' (W# w) = W# (foobar w)
+
 main :: IO ()
-main = undefined
+main = print $ foobar' 21
