5c7f4083d4a8e354ef7300bf37424d0e4983a1e8
0xd34df00d <0xd34df00d@gmail.com>
2020-02-15 15:40:57 -0500

    Some PoC that we can embed this stuff

--- src/Language/Asm/Inline.hs
+++ src/Language/Asm/Inline.hs
@@ -1,2 +1,11 @@
 module Language.Asm.Inline where
 
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+inlineAsm :: String -> Q [Dec]
+inlineAsm asmCode = do
+  addForeignSource LangAsm $ unlines [ asmCode
+                                     , "\tjmp *(%rbp)"
+                                     ]
+  pure []
