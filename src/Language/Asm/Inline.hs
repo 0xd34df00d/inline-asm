2eda6313e8e1e383465cb15e42f0b09ea77f8a97
0xd34df00d <0xd34df00d@gmail.com>
2020-02-15 18:15:34 -0500

    Some type-level stuff to aid in signature generation

--- src/Language/Asm/Inline.hs
+++ src/Language/Asm/Inline.hs
@@ -1,7 +1,34 @@
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, PolyKinds, TypeFamilies #-}
+
 module Language.Asm.Inline where
 
+import GHC.Prim
+import GHC.Types hiding (Type)
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+
+type family ArgRep a :: RuntimeRep where
+  ArgRep Int = 'IntRep
+  ArgRep Word = 'WordRep
+  ArgRep Float = 'FloatRep
+  ArgRep Double = 'DoubleRep
+
+class AsmArg a where
+  type UnboxedType a :: TYPE (ArgRep a)
+  unbox :: a -> UnboxedType a
+  rebox :: UnboxedType a -> a
+
+instance AsmArg Int where
+  type UnboxedType Int = Int#
+  unbox (I# w) = w
+  rebox = I#
+
+instance AsmArg Word where
+  type UnboxedType Word = Word#
+  unbox (W# w) = w
+  rebox = W#
 
 inlineAsm :: String -> Q [Dec]
 inlineAsm asmCode = do
