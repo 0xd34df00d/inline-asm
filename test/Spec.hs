19ca87d65538831ba94f19c6dc7896b1ed2dba31
0xd34df00d <0xd34df00d@gmail.com>
2020-02-16 00:45:12 -0500

    Test the tuple-returning function

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes #-}
+{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes, UnboxedTuples #-}
 
 import Test.Hspec
 import Test.QuickCheck
@@ -8,9 +8,12 @@ import Language.Asm.Inline
 
 defineAsmFun "timesTwo" [t| Int -> Int |] "add %rbx, %rbx"
 defineAsmFun "plusWord" [t| Int -> Int -> Int |] "add %r14, %rbx"
+defineAsmFun "swap" [t| Int -> Int -> (Int, Int) |] "xchg %rbx, %r14"
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
   describe "Basic functions" $ do
     it "timesTwo" $ property $ \num -> timesTwo num `shouldBe` num * 2
     it "plusWord" $ property $ \n1 n2 -> plusWord n1 n2 `shouldBe` n1 + n2
+  describe "Returning tuples" $
+    it "swap" $ property $ \n1 n2 -> swap n1 n2 `shouldBe` (n2, n1)
