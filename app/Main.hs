3c796d707f3a9bdd0001513ba56d803552923376
0xd34df00d <0xd34df00d@gmail.com>
2020-02-16 13:19:13 -0500

    Test the tuples function in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes #-}
+{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes, UnboxedTuples #-}
 
 module Main where
 
 import Language.Asm.Inline
 
-defineAsmFun "foobar" [t| Word -> Word |] "add %rbx, %rbx"
+defineAsmFun "swap" [t| Int -> Int -> (Int, Int) |] "xchg %rbx, %r14"
 
 main :: IO ()
-main = print $ foobar 21
+main = print $ swap 2 3
