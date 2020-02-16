3ecce725b7d03951dd30770cb90b44a343f81e04
0xd34df00d <0xd34df00d@gmail.com>
2020-02-15 21:49:23 -0500

    Throw up some tests

---
+++ test/Spec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes #-}
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Language.Asm.Inline
+
+defineAsmFun "timesTwo" [t| Int -> Int |] "add %rbx, %rbx"
+defineAsmFun "plusWord" [t| Int -> Int -> Int |] "add %r14, %rbx"
+
+main :: IO ()
+main = hspec $
+  describe "Basic functions" $ do
+    it "timesTwo works as expected" $ property $ \num -> timesTwo num `shouldBe` num * 2
+    it "plusWord works as expected" $ property $ \n1 n2 -> plusWord n1 n2 `shouldBe` n1 + n2
