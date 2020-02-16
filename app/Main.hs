fb680b7b35221d726b30a4b137a30b2f2f0cdcdd
0xd34df00d <0xd34df00d@gmail.com>
2020-02-15 19:35:51 -0500

    defineAsmFun is fully self-contained now

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MagicHash, GHCForeignImportPrim, UnliftedFFITypes #-}
+{-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes #-}
 
 module Main where
 
-import GHC.Prim
-import GHC.Word
-
 import Language.Asm.Inline
 
-defineAsmFun "foobar" [t| Word -> Word |] "\tadd %rbx, %rbx"
-
-foreign import prim "foobar_unlifted" foobar_unlifted :: Word# -> Word#
-
-foobar :: Word -> Word
-foobar (W# w) = W# (foobar_unlifted w)
+defineAsmFun "foobar" [t| Word -> Word |] "add %rbx, %rbx"
 
 main :: IO ()
 main = print $ foobar 21
