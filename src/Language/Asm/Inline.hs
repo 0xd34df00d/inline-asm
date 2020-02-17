c4d0d5e2f7e89abaa8a3473b3642e0d70fa859d2
0xd34df00d <0xd34df00d@gmail.com>
2020-02-17 00:20:54 -0500

    Added the assembly quasiquoter

--- src/Language/Asm/Inline.hs
+++ src/Language/Asm/Inline.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE MagicHash #-}
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances, FunctionalDependencies #-}
 {-# LANGUAGE DataKinds, PolyKinds, TypeFamilies #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveLift #-}
 
 module Language.Asm.Inline
 ( defineAsmFun
+, asm
 ) where
 
+import Control.Arrow
 import Control.Monad
+import Control.Monad.Except
+import Data.Either.Combinators
 import Data.Generics.Uniplate.Data
+import Data.List
 import GHC.Prim
 import GHC.Types hiding (Type)
 import Language.Haskell.TH
+import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
 
 class AsmArg a (rep :: RuntimeRep) (unboxedTy :: TYPE rep) | a -> rep, a -> unboxedTy where
@@ -43,6 +50,14 @@ instance AsmCode String where
   codeToString = id
   validateCode _ _ = pure ()
 
+instance AsmCode AsmQQParsed where
+  codeToString = asmBody
+  validateCode ty code =
+    check "arguments count mismatch" $ argsCount code == countArgs ty
+    where
+      check _ True = pure ()
+      check str False = throwError $ "Type error: " <> str
+
 defineAsmFun :: AsmCode c => String -> Q Type -> c -> Q [Dec]
 defineAsmFun name funTyQ asmCode = do
   addForeignSource LangAsm $ unlines [ ".global " <> asmName
@@ -65,6 +80,58 @@ defineAsmFun name funTyQ asmCode = do
   where
     name' = mkName name
     asmName = name <> "_unlifted"
+
+asm :: QuasiQuoter
+asm = QuasiQuoter { quoteExp = asmQE, quotePat = unsupported, quoteType = unsupported, quoteDec = unsupported }
+  where
+    unsupported = const $ error "Unsupported quasiquotation type"
+
+asmQE :: String -> Q Exp
+asmQE p = do
+  runIO $ print p
+  case parseAsmQQ p of
+       Left err -> error err
+       Right parsed -> [e| parsed |]
+
+data AsmQQParsed = AsmQQParsed
+  { argsCount :: Int
+  , asmBody :: String
+  } deriving (Show, Lift)
+
+parseAsmQQ :: String -> Either String AsmQQParsed
+parseAsmQQ = findSplitter
+         >=> (pure . first words)
+         >=> substituteArgs
+  where
+    findSplitter p = case break (== '|') p of
+                          (vars, '|' : body) -> pure (vars, body)
+                          _ -> throwError "Unable to find variable section separator"
+
+substituteArgs :: ([String], String) -> Either String AsmQQParsed
+substituteArgs (args, contents) = AsmQQParsed (length args) <$> go contents
+  where
+    go ('$' : '{' : rest)
+      | (arg, '}' : rest') <- break (== '}') rest = do
+        idx <- if retPrefix `isPrefixOf` arg
+                  then pure $ read (drop (length retPrefix) arg)
+                  else maybeToRight ("Unknown argument: `" <> trim arg <> "`") $ elemIndex (trim arg) args
+        reg <- argIdxToReg idx
+        (('%' : reg) <>) <$> go rest'
+      | otherwise = throwError $ "Unable to parse argument: " <> take 20 rest <> "..."
+    go (x : xs) = (x :) <$> go xs
+    go [] = pure []
+
+    retPrefix = "ret"
+
+argIdxToReg :: Int -> Either String String
+argIdxToReg 0 = pure "rbx"
+argIdxToReg 1 = pure "r14"
+argIdxToReg n = throwError $ "Unsupported register index: " <> show n
+
+trim :: String -> String
+trim = pass . pass
+  where
+    pass = reverse . dropWhile (== ' ')
 
 mkFunD :: String -> Name -> Type -> Q Dec
 mkFunD funName importedName funTy = do
