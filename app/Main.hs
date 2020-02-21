48be2f32c888f855a07c1a6c708ffc4717aa35e1
0xd34df00d <0xd34df00d@gmail.com>
2020-02-21 15:23:07 -0500

    Some examples in app/Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,24 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
 {-# LANGUAGE GHCForeignImportPrim, UnliftedFFITypes, UnboxedTuples #-}
 
 module Main where
 
 import Language.Asm.Inline
+import Language.Asm.Inline.QQ
 
 defineAsmFun "swap" [t| Int -> Int -> (Int, Int) |] "xchg %rbx, %r14"
 
+defineAsmFun "swap2p1"
+  [t| Int -> Int -> (Int, Int) |]
+  [asm| a b |
+  xchg ${a}, ${b}
+  add $1, ${b}
+  |]
+
+defineAsmFun "testInt" [t| Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int |] "int $3"
+defineAsmFun "testDouble" [t| Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double |] "int $3"
+
 main :: IO ()
-main = print $ swap 2 3
+main = do
+  print $ testInt 0 1 2 3 4 5 6
+  print $ testDouble 1 1 0 0 0 0 1
