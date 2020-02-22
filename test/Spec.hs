ae1904cce4352fbdee273a53fc49052efb614b75
0xd34df00d <0xd34df00d@gmail.com>
2020-02-21 19:43:47 -0500

    Better test names

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,14 +6,13 @@ import Test.QuickCheck
 
 import Language.Asm.Inline
 
-defineAsmFun "timesTwo" [t| Int -> Int |] "add %rbx, %rbx"
-defineAsmFun "plusWord" [t| Int -> Int -> Int |] "add %r14, %rbx"
-defineAsmFun "swap" [t| Int -> Int -> (Int, Int) |] "xchg %rbx, %r14"
+defineAsmFun "timesTwoInt" [t| Int -> Int |] "add %rbx, %rbx"
+defineAsmFun "plusInt" [t| Int -> Int -> Int |] "add %r14, %rbx"
+defineAsmFun "swapInts" [t| Int -> Int -> (Int, Int) |] "xchg %rbx, %r14"
 
 main :: IO ()
-main = hspec $ do
-  describe "Basic functions" $ do
-    it "timesTwo" $ property $ \num -> timesTwo num `shouldBe` num * 2
-    it "plusWord" $ property $ \n1 n2 -> plusWord n1 n2 `shouldBe` n1 + n2
-  describe "Returning tuples" $
-    it "swap" $ property $ \n1 n2 -> swap n1 n2 `shouldBe` (n2, n1)
+main = hspec $
+  describe "defineAsmFun (without QQ, with Ints)" $ do
+    it "timesTwo" $ property $ \num -> timesTwoInt num `shouldBe` num * 2
+    it "plusWord" $ property $ \n1 n2 -> plusInt n1 n2 `shouldBe` n1 + n2
+    it "swap returns a tuple properly" $ property $ \n1 n2 -> swapInts n1 n2 `shouldBe` (n2, n1)
